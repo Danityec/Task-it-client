539bcbf8ead6159ac9e21d735a70a600e7559bf1
halpert-keren <halpertkeren@gmail.com>
2021-01-26 17:02:04 +0200

    header fixes

--- src/Components/shared/Header.js
+++ src/Components/shared/Header.js
@@ -7,35 +7,24 @@ const Header = () => {
     const [open, setOpen] = useState(false);
     const anchorRef = useRef(null);
 
-    const handleToggle = () => {
-        setOpen((prevOpen) => !prevOpen);
-    };
-
-    const handleClose = (event) => {
-        if (anchorRef.current && anchorRef.current.contains(event.target)) {
-            return
-        }
-        setOpen(false);
-    };
-
     return (
         <div className={"header"}>
             <div className={'header-content'}>
                 <NavLink exact to="/" className={'logo'}/>
                 <div>
                     <Avatar className={'user-avatar'}
-                            ref={anchorRef} aria-controls={open ? 'menu-list-grow' : undefined}
-                            aria-haspopup="true" onClick={handleToggle}>
+                            ref={anchorRef} aria-controls={open ? 'menu-list' : undefined}
+                            aria-haspopup="true" onClick={() => setOpen(prevOpen => !prevOpen)}>
                     </Avatar>
-                    <Popper open={open} anchorEl={anchorRef.current} role={undefined} placement={'bottom-end'} transition disablePortal>
-                        {({ TransitionProps, placement }) => (
-                            <Grow {...TransitionProps} style={{ transformOrigin: placement === 'bottom-end' ? 'right top' : 'right bottom'}}>
+                    <Popper open={open} anchorEl={anchorRef.current} placement={'bottom-end'} transition>
+                        {({TransitionProps}) => (
+                            <Grow {...TransitionProps} style={{transformOrigin: 'right top'}}>
                                 <Paper style={{backgroundColor: '#2A73CC'}}>
-                                    <ClickAwayListener onClickAway={handleClose}>
-                                        <MenuList id="menu-list-grow">
-                                            <MenuItem><NavLink className={'menu-list-text'} to='/account'>Account</NavLink></MenuItem>
-                                            <MenuItem><NavLink className={'menu-list-text'} to='/my-reviews'>My Reviews</NavLink></MenuItem>
-                                            <MenuItem><NavLink className={'menu-list-text'} to='/logout'>Logout</NavLink></MenuItem>
+                                    <ClickAwayListener onClickAway={() => setOpen(false)}>
+                                        <MenuList id="menu-list">
+                                            <MenuItem><NavLink className={'menu-list-item'} to='/account'>Account</NavLink></MenuItem>
+                                            <MenuItem><NavLink className={'menu-list-item'} to='/my-reviews'>My Reviews</NavLink></MenuItem>
+                                            <MenuItem><NavLink className={'menu-list-item'} to='/logout'>Logout</NavLink></MenuItem>
                                         </MenuList>
                                     </ClickAwayListener>
                                 </Paper>
