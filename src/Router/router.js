804bd487364e1e66b3f03f38c002d6462b6f467c
Danityec <Danityec@gmail.com>
2021-01-26 17:30:48 +0200

    system routes

--- src/Router/router.js
+++ src/Router/router.js
@@ -2,12 +2,26 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import Header from "../Components/shared/Header";
 import Footer from "../Components/shared/Footer";
+import Menu from "../Components/shared/Menu";
+import HomePage from "../Components/homePage/HomePage";
+import TemplateList from "../Components/template/TemplateList";
+import ChatList from "../Components/chat/ChatList";
+import Chat from "../Components/chat/Chat";
+import Task from "../Components/task/Task";
+
 
 const ReactRouter = () => {
     return (
         <>
             <Header/>
-            <Route exact path='/' component={'ss'}/>
+            <Menu/>
+            
+            <Route exact path='/' component={HomePage}/>
+            <Route path='/new-task' component={TemplateList}/>
+            <Route path='/chats' component={ChatList}/>
+            <Route path='/chat' component={Chat}/>
+            <Route path='/task' component={Task}/>
+
             <Footer/>
         </>
     )
