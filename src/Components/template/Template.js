17c298654d23c024820dfa97f7692dedb231d856
Danityec <Danityec@gmail.com>
2021-01-28 19:23:04 +0200

    update on Template List component

--- src/Components/template/Template.js
+++ src/Components/template/Template.js
@@ -4,27 +4,29 @@ import './Template.css';
 import Button from '@material-ui/core/Button';
 
 
-
-export default function Template(props){
-
-    function add(){}
-
-
+const Template = (props) => {
+   
+   
     return (
         <div className="template">
+            
             <div className="titleTask">
-                <div className="nameTask"/>
-                <Button className="iconeChoose" fontSize="small" aria-label="edit" onClick={add} variant="contained" style={{ padding: "1px"}} color="primary">Choose</Button >
+                <div className="nameTask">{props.item.name}</div>
+                <Button className="iconeChoose" fontSize="small" aria-label="edit" variant="contained" style={{ padding: "1px"}} color="primary">Choose</Button >
             </div>
-            <div className="titleCategory">
-                <div className="nameCategory"/>
 
+            <div className="titleCategory">
+                <div className="nameCategory" >{props.item.category}</div>
             </div>
+
             <div className="titleSubTask">
-                <div className="nameSubTask"/>
+                {/* <div className="nameSubTask" >{props.item.subTask}</div> */}
             </div>
+            
         </div>
-    )
+        )   
+    
     
 }
 
+export default Template;
