210362297d42e42c9395989d29142bae6e4e2f3b
halpert-keren <halpertkeren@gmail.com>
2021-01-30 19:37:32 +0200

    create chat functionality

--- src/Components/chat/ChatList.js
+++ src/Components/chat/ChatList.js
@@ -5,23 +5,35 @@ import {ButtonBase, IconButton} from "@material-ui/core";
 import List from "../shared/List";
 import Popup from "../shared/Popup";
 import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import ArrowForwardIosRoundedIcon from "@material-ui/icons/ArrowForwardIosRounded";
 
 const userId = '5fecb592690ca7935ccfd762'
-const userID1 = '5fecb592690ca7935ccfd762'
 const userID2 = '5fea10ee690ca7935c117922'
 
 const ChatList = (props) => {
     const [chatList, setChatList] = useState([]);
     const [titleList, setTitleList] = useState({});
     const [open, setOpen] = useState(false);
-    const [emailChat, setEmailChat] = useState("");
+    const [emailList, setEmailList] = useState([]);
+    const [emailValue, setEmailValue] = useState(null);
 
     useEffect(() => {
         fetch(`http://127.0.0.1:3000/api/chats?userID=${userId}`)
             .then(response => response.json())
             .then(result => setChatList(result))
     }, [])
+
+    useEffect(() => {
+        fetch(`http://127.0.0.1:3000/api/users`)
+            .then(response => response.json())
+            .then(result => {
+                result.forEach(user => {
+                    setEmailList(prevArray => [...prevArray, {title: user['email']}])
+                })
+            })
+    }, [chatList])
+
 
     useEffect(() => {
         let users = []
@@ -43,32 +55,31 @@ const ChatList = (props) => {
         })
     }, [chatList])
 
-   
-    const addNewChat = () => { 
-        const data = { email: emailChat, userID: userID1, userID: userID2};
-        console.log(data)
-            fetch(`http://localhost:3000/api/chats/`, {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(data),
+    const addNewChat = () => {
+        fetch(`http://127.0.0.1:3000/api/users?email=${emailValue.title}`)
+            .then(response => response.json())
+            .then(result => {
+                const body = {userID1: userId, userID2: result['_id']};
+                fetch(`http://localhost:3000/api/chats/`, {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(body),
+                })
+                    .then(response => response.json())
+                    .then(result => {
+                        setOpen(false)
+                        setChatList(result)
+                    });
             })
-                .then(response => response.json())
-                .then(result => {handleClose()});
+
 
     }
-
-    const handleClose = () => {
-        setOpen(false);
-
-    };
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
 
     return (
         <>
             <Menu goBack={true}>
-                <ButtonBase centerRipple={true} onClick={handleClickOpen}><p style={{width: '150px'}}>New Chat</p></ButtonBase>
+                <ButtonBase centerRipple={true} onClick={() => setOpen(true)}><p style={{width: '150px'}}>New Chat</p>
+                </ButtonBase>
             </Menu>
             <div className={'chat-list'}>
                 <List dataList={chatList} titleList={titleList} pathName={'/chat'}>
@@ -76,18 +87,20 @@ const ChatList = (props) => {
                         <ArrowForwardIosRoundedIcon/>
                     </IconButton>
                 </List>
-
-                <Popup onSubmit={addNewChat} title={"New Chat"} text={"Open a new chat with another TaskIt user"} open={open}>
-                    <TextField key={1} className="inputEmail"
-                            autoFocus
-                            margin="dense"
-                            id="Email"
-                            label="Email"
-                            type="Email"
-                            onChange={e => setEmailChat(e.target.value)}
-                            fullWidth
-                            value={emailChat}>
-                    </TextField>
+                <Popup onSubmit={addNewChat} closePopup={() => {
+                    setOpen(false)
+                }} title={"New Chat"} open={open} isDelete={false}>
+                    <p>open a new chat with another TaskIt user</p>
+                    {/*<TextField className="chat-email-input" label="Email" onChange={e => setEmailChat(e.target.value)}*/}
+                    {/*           fullWidth value={emailChat}/>*/}
+                    <Autocomplete
+                        style={{width: '100%', paddingTop: '5%'}}
+                        options={emailList}
+                        getOptionLabel={(emailList) => emailList.title}
+                        value={emailValue}
+                        onChange={(e, newValue) => {setEmailValue(newValue)}}
+                        renderInput={(params) => <TextField {...params} label="Email"/>}
+                    />
                 </Popup>
             </div>
         </>
