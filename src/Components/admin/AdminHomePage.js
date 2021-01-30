e92080ad320a40aee20c41eb2a99f2794db3816a
Danityec <Danityec@gmail.com>
2021-01-30 21:23:25 +0200

    update admin HomePage and AdminTask

--- src/Components/admin/AdminHomePage.js
+++ src/Components/admin/AdminHomePage.js
@@ -52,9 +52,9 @@ const AdminHomePage = (props) => {
     }
 
     const addNewTemaplate = () => {
-        const data = { name: templateName, category: templateCategory };
+        const data = { name: templateName, category: templateCategory, };
         console.log(data)
-        fetch(``, {
+        fetch(`http://localhost:3000/api/tasks/`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data),
