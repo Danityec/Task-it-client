eb488367ea0a762b19ed3c7650d4e53d59d1484b
Danityec <Danityec@gmail.com>
2021-02-05 17:50:45 +0200

    update new fuction googleSucces in componene Login

--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import axios from 'axios';
+
 import './Login.css'
 import { GoogleLogin } from 'react-google-login'
+
+const googleSuccess = async (response) => {
+  const tmp = {
+    token: response.tokenId
+  }
+
+  axios.post('http://localhost:3000/authLogin', tmp, { withCredentials: true, credentials: 'include' })
+    .then(res => {
+      console.log(res);
+      console.log(res.data)
+    })
+    .catch(err => console.log(err))
+}
+
+const googleFailure = (response) => {
+  console.log(response);
+}
 
 const Login = (props) => {
      return (
         <div className={'login-page'}>
           <GoogleLogin className={'login-btn'}
-            clientId={'198399782561-j1p0mljmh170ufum7em3qvm4cfvbqnsu.apps.googleusercontent.com'}
-            onSuccess={(response)=>console.log(response)}
+            clientId='554171649210-i97q2kqu31t4hg021qdpmjn9kbobor0h.apps.googleusercontent.com'
+            onSuccess={googleSuccess}
             onFailure={()=>console.log('failure')}
           />
         </div>
