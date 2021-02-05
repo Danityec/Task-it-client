4b5cc20c871b4edd7f8b0e20e53877a2caa6387c
halpert-keren <halpertkeren@gmail.com>
2021-02-05 11:01:17 +0200

    google login button and dependency

--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './Login.css'
+import { GoogleLogin } from 'react-google-login'
 
 const Login = (props) => {
      return (
-        <>
-          hi
-        </>
+        <div className={'login-page'}>
+          <GoogleLogin className={'login-btn'}
+            clientId={'198399782561-j1p0mljmh170ufum7em3qvm4cfvbqnsu.apps.googleusercontent.com'}
+            onSuccess={(response)=>console.log(response)}
+            onFailure={()=>console.log('failure')}
+          />
+        </div>
      )
 }
 
