7d3cdb9bbbe8c0d46060a1e68a654680d65ce944
halpert-keren <halpertkeren@gmail.com>
2021-02-17 11:53:53 +0200

    quote box click away listener

--- src/Components/shared/Footer.js
+++ src/Components/shared/Footer.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import CopyrightIcon from '@material-ui/icons/Copyright';
 import './Footer.css'
 import Popper from '@material-ui/core/Popper';
-import {ButtonBase} from '@material-ui/core';
+import {ButtonBase, ClickAwayListener} from '@material-ui/core';
 
 
 const Footer = () => {
@@ -15,8 +15,9 @@ const Footer = () => {
         fetch(`http://localhost:3000/quotes`, {credentials: 'include'})
             .then(response => response.json())
             .then(result => {
-                setQuote(result[0].q);
-                setAuthor(result[0].a);
+                setQuote(result[0].q)
+                setAuthor(result[0].a)
+                setOpen(true)
             })
     }
 
@@ -29,22 +30,23 @@ const Footer = () => {
             <div className={'buttonDiv'}>
                 <ButtonBase className={'buttonBase'} type="button" style={{backgroundColor: '#34B467'}}
                             onClick={(e) => {
-                                quoteAPI();
                                 setAnchorEl(e.currentTarget);
-                                setOpen(!open);
+                                if (open) setOpen(false)
+                                else quoteAPI();
                             }}>
                     <p>New Quote</p>
                 </ButtonBase>
             </div>
-            <Popper open={open} anchorEl={anchorEl} placement={'top-end'}
-            style={{ minWidth: '200px', width: 'fit-content'}}>
-                <div className={'popper'}>
-                    <p>Quote of the Day</p>
-                    <p className={'quote'}>{quote}</p>
-                    <p className={'author'}>{author}</p>
-                </div>
-            </Popper>
-
+            <ClickAwayListener onClickAway={() => setOpen(false)}>
+                <Popper open={open} anchorEl={anchorEl} placement={'top-end'}
+                        style={{minWidth: '200px', width: 'fit-content'}}>
+                    <div className={'popper'}>
+                        <p>Quote of the Day</p>
+                        <p className={'quote'}>{quote}</p>
+                        <p className={'author'}>{author}</p>
+                    </div>
+                </Popper>
+            </ClickAwayListener>
         </>
     )
 }
