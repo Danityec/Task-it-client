2fda3742f770bad4cc405a29dc53cc6662bcc074
halpert-keren <halpertkeren@gmail.com>
2021-02-22 15:29:13 +0200

    url router fix

--- src/Components/shared/List.js
+++ src/Components/shared/List.js
@@ -6,7 +6,7 @@ const List = (props) => {
     const eachItem = (item) => {
         return (
             <ListItem action={props.action} checkboxes={props.checkboxes}
-                      checkboxeToggle={props.checkboxToggle}
+                      checkboxToggle={props.checkboxToggle}
                       key={item._id} item={item} itemTitle={props.titleList[item._id]} pathName={props.pathName}>
                 {props.children}
             </ListItem>
