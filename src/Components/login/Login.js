c29c146e5a148a1c0ea750997035341f655ba0af
halpert-keren <halpertkeren@gmail.com>
2021-02-22 08:54:57 +0200

    cookie domain fix

--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -20,8 +20,8 @@ const Login = (props) => {
             .then(response => response.json())
             .then(result => {
                 const cookiePromise = new Promise((resolve, reject) => {
-                    // setCookie('user', result)
-                    setCookie('user', result ,{ domain: '.task--it.herokuapp.com' });
+                    setCookie('user', result)
+                    // setCookie('user', result ,{ domain: '.task--it.herokuapp.com' });
                     // setCookie('user-front', result ,{ domain: '.taskitapp.netlify.app' });
 
                     resolve()
